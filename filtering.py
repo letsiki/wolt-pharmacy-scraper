f73ae8545ce84f92f42d47a1533bd4fe041f54c9
alexa <letsiki@gmail.com>
2024-07-09 00:56:27 +0300

    filtering now has a multiprocessor process that calculates levenstein duplicates and can even further reduce the list of items. Edited todos

--- filtering.py
+++ filtering.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from multiprocessing import Pool, cpu_count, freeze_support
 import pandas as pd
-from fuzzywuzzy import process, fuzz
+from Levenshtein import ratio as levenshtein_ratio
 
 # Load the CSV file into a DataFrame
 df_items = pd.read_csv('items_20240708_031709.csv')
 df_labels = pd.read_csv('top_labels_20240708231535.csv')
-print(df_labels)
+# print(df_labels)
 # Create a list of labels to check
 labels_to_check = df_labels['Label'].tolist()
 
@@ -28,19 +29,37 @@ filename = f'filtered_items_{timestamp}.csv'
 
 print(f'DataFrame saved to {filename}')
 
-# Function to find similar entries and keep only one
-def find_and_remove_similar(df, threshold=90):
-    # Calculate similarity for each pair of descriptions
-    duplicates = []
-    for i, desc1 in enumerate(df['Description']):
-        for j, desc2 in enumerate(df['Description']):
-            if i != j and fuzz.ratio(desc1, desc2) >= threshold:
-                duplicates.append(desc2)
+def calculate_similarity(args):
+    i, desc, description_column, threshold = args
+    indices_to_drop = []
+    for j in range(i + 1, len(description_column)):
+        if levenshtein_ratio(desc, description_column[j]) >= threshold:
+            indices_to_drop.append(j)
+    return indices_to_drop
 
-    # Drop the duplicate entries
-    sorted_filtered_df_items.drop_duplicates(subset='Description', inplace=True)
+def find_and_remove_similar(df, threshold=0.9):
+    description_column = df['Description'].tolist()
+    indices_to_drop = []
 
-# Find and remove similar entries
-find_and_remove_similar(sorted_filtered_df_items)
+    # Create a pool of processes
+    with Pool(processes=cpu_count()) as pool:
+        # Execute the tasks in parallel
+        results = pool.map(calculate_similarity, [(i, desc, description_column, threshold) for i, desc in enumerate(description_column)])
 
-print(sorted_filtered_df_items)
+        # Collect the results
+        for result in results:
+            indices_to_drop.extend(result)
+
+    # Drop duplicates
+    print(df.drop(indices_to_drop).reset_index(drop=True))
+    
+
+
+if __name__ == '__main__':
+    freeze_support()  # Needed for Windows when creating frozen executables
+    # Find and remove similar entries
+    find_and_remove_similar(sorted_filtered_df_items)
+
+    # print(sorted_filtered_df_items.reset_index(drop=True))
+
+# print(sorted_filtered_df_items.shape)
